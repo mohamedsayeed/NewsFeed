e006a8675c22535f5ee2c4bd82586bcc6149c726
Mohamed Sayed <mohamedsayeed.29@gmail.com>
2017-11-10 19:37:05 +0200

    add search

--- app/src/main/java/com/link/newsfeed/ArticleAdapter.java
+++ app/src/main/java/com/link/newsfeed/ArticleAdapter.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,15 +20,23 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
-public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> implements Filterable {
     private Context mContext;
-    private ArrayList<Article> mArticles;
+    private ArrayList<Article> mArticles = new ArrayList<>();
+
+    private List<Article> ArticlesList = null;
+    private List<Article> filteredArticlesList;
+    private ArticleFilter articleFilter;
+
 
     ArticleAdapter(Context context, ArrayList<Article> articles) {
         mContext = context;
         mArticles = articles;
+        this.ArticlesList = articles;
+        this.filteredArticlesList = new ArrayList<>();
     }
 
     @Override
@@ -59,6 +69,13 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         return mArticles.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        if (articleFilter == null)
+            articleFilter = new ArticleFilter(this, ArticlesList);
+        return articleFilter;
+    }
+
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView ArticleImage;
@@ -83,11 +100,57 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
                 case R.id.article_card:
                     Intent ArticleDetailsIntent = new Intent(mContext, ArticleDetails.class);
                     Bundle extras = new Bundle();
-                    extras.putParcelable("Article",mArticles.get(getAdapterPosition()));
+                    extras.putParcelable("Article", mArticles.get(getAdapterPosition()));
                     ArticleDetailsIntent.putExtras(extras);
                     mContext.startActivity(ArticleDetailsIntent);
                     break;
             }
+        }
+    }
+
+
+    private static class ArticleFilter extends Filter {
+
+        private final ArticleAdapter adapter;
+
+        private List<Article> originalList = new ArrayList<>();
+
+        private final List<Article> filteredList;
+
+        private ArticleFilter(ArticleAdapter adapter, List<Article> originalList) {
+            super();
+            this.adapter = adapter;
+            this.originalList = new ArrayList<>(originalList);
+            this.filteredList = new ArrayList<>();
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            filteredList.clear();
+            final FilterResults results = new FilterResults();
+
+            if (constraint.length() == 0) {
+                filteredList.addAll(originalList);
+            } else {
+                final String filterPattern = constraint.toString().toLowerCase().trim();
+                for (final Article article : originalList) {
+                    if (article.getTitle().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(article);
+                    }
+                }
+            }
+            results.values = filteredList;
+            results.count = filteredList.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            adapter.filteredArticlesList.clear();
+            adapter.filteredArticlesList.addAll((ArrayList<Article>) results.values);
+            adapter.mArticles.clear();
+            adapter.mArticles.addAll(filteredList);
+            adapter.notifyDataSetChanged();
         }
     }
 }
--- app/src/main/java/com/link/newsfeed/MainActivity.java
+++ app/src/main/java/com/link/newsfeed/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
 import com.link.newsfeed.connection.ConnectionManager;
@@ -29,11 +32,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-    private NavigationView mNavView;
     private RecyclerView mRecyclerArticles;
     private ArticleAdapter mArticleAdapter;
     private ProgressDialog mProgressDialog;
     private ArrayList<Article> mArticlesArray = new ArrayList<>();
+
+    SearchView searchView;
 
 
     @Override
@@ -78,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void initializeDrawer() {
         Toolbar mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        mNavView = findViewById(R.id.nav_view);
+        NavigationView mNavView = findViewById(R.id.nav_view);
         mNavView.setNavigationItemSelectedListener(this);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
@@ -101,17 +105,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        Search();
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item))
-            return true;
-        if (item.getItemId() == R.id.action_search)
-            return true;
-
-        return super.onOptionsItemSelected(item);
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -136,5 +138,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void Search() {
+        final AutoCompleteTextView searchEditText = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setHint("Search");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // Here is where we are going to implement the filter logic
+                mArticleAdapter.getFilter().filter(searchEditText.getText());
+                mRecyclerArticles.setAdapter(mArticleAdapter);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String filterString) {
+                mArticleAdapter.getFilter().filter(searchEditText.getText());
+                return true;
+            }
+        });
     }
 }
